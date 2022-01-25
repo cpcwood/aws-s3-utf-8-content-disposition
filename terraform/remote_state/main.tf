963c227e13cb33af9d40ead6148b1ee7aa731b21
cpcwood <cpcwood@outlook.com>
2022-01-25 22:12:54 +0000

    add backend state bucket

---
+++ terraform/remote_state/main.tf
@@ -0,0 +1,48 @@
+# builds:
+#   - backend s3 bucket to store main application terraform state
+#   - dynamodb table to lock state during changes
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.73"
+    }
+  }
+  required_version = ">= 1.1.4"
+}
+
+provider "aws" {
+  region = var.tf_state_bucket_region
+  profile = "ecs-rollback-hello-world"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.tf_state_s3_bucket
+  acl = "private"
+  tags = {}
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls = true
+  block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls = true
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name = var.tf_state_lock_table
+  read_capacity = 1
+  write_capacity = 1
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ terraform/remote_state/variables.tf
@@ -0,0 +1,14 @@
+variable "tf_state_bucket_region" {
+  description = "AWS region the backend s3 bucket is created in"
+  default = "eu-west-2"
+}
+
+variable "tf_state_s3_bucket" {
+  description = "AWS S3 bucket in which to store main application terraform state"
+  default = "aws-s3-utf-8-content-disposition-tf-state"
+}
+
+variable "tf_state_lock_table" {
+  description = "AWS DynamoDB terraform state lock table name"
+  default = "aws-s3-utf-8-content-disposition-tf-state-lock-table"
+}
